f1c7f426447f4f5efb0cb9b91184237fc239e4a4
morimoto <morimotosemiac@gmail.com>
2018-07-14 13:54:31 +0900

    making simple perceptron

--- perceptron.py
+++ perceptron.py
@@ -9,11 +9,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-rng= np.random.RandomState(13)
+rng= np.random.RandomState(123)
 
 d = 2 #データの次元
-N = 100 #各パターンのデータ数
-mean = -5 #ニューロンが発火するデータの平均値
+N = 10 #各パターンのデータ数
+mean = 5 #ニューロンが発火するデータの平均値
 
 x1 = rng.randn(N, d) + np.array([0, 0])
 x2 = rng.randn(N, d) + np.array([mean, mean])
@@ -24,11 +24,11 @@ x = np.concatenate((x1, x2), axis= 0)
 w = np.zeros(d)
 b = 0
 
+def step(p): #ステップ関数
+    return 1 * (p > 0) # p>0なら1、p=<0なら0になる関数
+
 def y(x): #出力 y=f(w*x + b)を表現
     return step(np.dot(w, x) + b)
-
-def step(x): #ステップ関数
-    return 1 * (x > 0)
 
 def t(i): #パラメーターを更新するための出力値
     if i < N:
@@ -40,25 +40,28 @@ def t(i): #パラメーターを更新するための出力値
 while True:
     classified = True
     for i in range(N*2):
-        delta_w= (t(i) - y(x[i]))*x[i]
+        delta_w= (t(i) - y(x[i])) * x[i]
         delta_b= (t(i) - y(x[i]))
         w += delta_w
         b += delta_b
         classified *= all((delta_w == 0)*(delta_b == 0)) #all(int):全ての要素がTrueの時にTrueを返す
-    if classified:
+    if classified == True:
         break
 print("wの予測値：{}".format(w))
 print("bの予測値：{}".format(b))
 
-plt.scatter(x1[:,0],x1[:,1])
-plt.scatter(x2[:,0],x2[:,1])
+plt.scatter(x1[:,0], x1[:,1])
+plt.scatter(x2[:,0], x2[:,1])
 
-X= np.linspace(-7,3,100)
+plt.figure()
+
+X= np.linspace(-3,6,100)
 Y= []
 for i in X:
-    Y.append(w[0]*i + w[1]*i + b)
+    Y.append((-w[1]/w[0])*i + b/w[0])
  
 plt.plot(X,Y)
+plt.scatter(x[:,0],x[:,1])
 plt.show()
         
         
